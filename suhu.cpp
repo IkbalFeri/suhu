bc75d343e55b54cffc46dd58d8707dbf200e5319
IkbalFeri <ikbalferiamda@gmail.com>
2024-10-07 15:21:15 +0700

    Update suhu.cpp

--- suhu.cpp
+++ suhu.cpp
@@ -6,4 +6,19 @@ const double FAKTOR_C_TO_F = 9.0 / 5.0;
 const double FAKTOR_C_TO_R = 4.0 / 5.0;
 const double OFFSET_C_TO_K = 273.0;
 
+double konversiCKeF(double nilai);
+double konversiCKeR(double nilai);
+double konversiCKeK(double nilai);
+double konversiFKeC(double nilai);
+double konversiFKeR(double nilai);
+double konversiFKeK(double nilai);
+double konversiRKeC(double nilai);
+double konversiRKeF(double nilai);
+double konversiRKeK(double nilai);
+double konversiKKeC(double nilai);
+double konversiKKeR(double nilai);
+double konversiKKeF(double nilai);
+void konversiSuhu(char skalaAwal, char skalaAkhir, double nilai);
+
+
 
